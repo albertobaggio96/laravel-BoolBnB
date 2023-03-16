64843b837f88c1bfa75bc4f8a177341ac7995cd4
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-16 15:05:02 +0100

    model property

---
+++ app/Models/Property.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+
+    protected $fillable= [
+        'user_id',
+        'title',
+        'description',
+        'night_price',
+        'n_beds',
+        'n_rooms',
+        'cover_img',
+        'mq',
+        'visible',
+        'address',
+        'latitude',
+        'longitude'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
+}
