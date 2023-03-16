906a0067b6e22ceb557ad5347ed7d31d79c73a1d
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-16 16:45:25 +0100

    added crud

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Property;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Str;
 class PropertyController extends Controller
 {
     /**
@@ -25,20 +25,25 @@ class PropertyController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Property $property)
     {
-        //
+        return view('admin.properties.create', compact('property'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request  $property
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $property)
     {
-        //
+        $data = $property->all();
+        $newProperty = new Property();
+        $newProperty->fill($data);
+        $newProperty->slug = Str::slug($newProperty->title);
+        $newProperty->save();
+        return redirect()->route('admin.properties.show', $newProperty->slug);
     }
 
     /**
@@ -55,12 +60,12 @@ class PropertyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Property $property
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Property $property)
     {
-        //
+        return view('admin.properties.edit', compact('property'));
     }
 
     /**
@@ -70,19 +75,24 @@ class PropertyController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Property $property)
     {
-        //
+        $data = $request->all();
+
+        $property->slug = Str::slug($property->title);
+        $property->update($data);
+        return redirect()->route('admin.properties.show', $property->slug);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Property $property
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Property $property)
     {
-        //
+        $property->delete();
+        return redirect()->route('admin.properties.index');
     }
 }
