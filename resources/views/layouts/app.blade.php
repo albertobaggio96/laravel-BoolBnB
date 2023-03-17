c7cce54c6943284aadd7d2eb814aaf66ee46b790
cerry24 <samuelecerretti@gmail.com>
2023-03-17 15:32:43 +0100

    add nav link for properties index into app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('admin.properties.index') }}">{{ __('Properties') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
