c892cdbde0f3bc10af40a7ff52a830c57a63aacd
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-20 16:48:19 +0100

    install sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
+import 'sweetalert2/src/sweetalert2.scss'
 import '~resources/scss/app.scss';
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
