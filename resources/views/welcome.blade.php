898d0401060edbd4e917d687e3d31138aaadae9b
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-20 09:23:39 +0100

    edit welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="text-center">
-        <h1>Benvenuto {{Auth::user()->name ?? 'user'}}</h1>
+        <h1>Benvenuto {{Auth::user()->name ?? ''}}</h1>
     </div>
 @endsection
