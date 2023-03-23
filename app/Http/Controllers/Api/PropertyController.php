2615b6d6eeb1c75d3f06b5fb6ad85761fd6bf22a
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-03-23 16:22:03 +0100

    draft api with params

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -11,24 +11,30 @@ use App\Models\Service;
 use App\Models\Sponsorship;
 
 class PropertyController extends Controller {
-    
-    public function index() {
-        //$properties = Property::all();
-        $properties = Property::with('services', 'sponsorships', 'images', 'user', 'views')->get();
-        return response()->json([
-            'success' => true,
-            'results' => $properties
-        ]);
-    }
-
 
     public function show(Property $property){
-        //$property = Property::where('slug', $property->slug)->get(); 
-        //$property = Property::with('services', 'sponsorships', 'images', 'user', 'views')->findOrFail($property->id)->get();
         $property = Property::with('services', 'sponsorships', 'images', 'user', 'views')->findOrFail($property->id);
         return response()->json([
             'success' => true,
             'results' => $property
+        ]);
+    }
+
+    public function index(Property $property, Request $request){
+        $params = $request->query();
+        //$arrayWhere = [
+            //['title', 'Like', '%' . $params['title'] . '%'],
+        //];
+        //if ($params['min_beds']) { array_push($arrayWhere,  ['n_beds', '>=', $params['min_beds']]); };
+        //if ($params['max_beds']) { array_push($arrayWhere,  ['n_beds', '<=', $params['max_beds']]); };
+        
+        $properties = Property::with('services', 'sponsorships', 'images', 'user', 'views')
+                    ->get();
+
+        return response()->json([
+            'success' => true,
+            'parmas' => $params,
+            'results' => $properties
         ]);
     }
 }
