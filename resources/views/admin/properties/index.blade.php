2b56864578b90a620ae371b90c3a18e1be89231c
cerry24 <samuelecerretti@gmail.com>
2023-03-28 12:10:36 +0200

    add message if user has no properties + opacity where visible is false

--- resources/views/admin/properties/index.blade.php
+++ resources/views/admin/properties/index.blade.php
@@ -21,7 +21,7 @@
     </form>
   </div>
   <div class="table-responsive-sm">
-  <table class="table table-striped table-hover">
+  <table class="table table-hover">
     <thead>
       <tr>
         <th scope="col">Titolo</th>
@@ -34,26 +34,28 @@
         <th scope="col">Azioni utente</th>
     </thead>
     <tbody class="table-group-divider">
-      @foreach ($properties as $property)
-        <tr>
-            <td>{{ $property->title }}</td>
-            <td class="custom-responsive-sm">{{ $property->night_price }}</td>
-            <td class="custom-responsive-sm">{{ $property->n_beds }}</td>
-            <td class="custom-responsive-sm">{{ $property->n_rooms }}</td>
-            <td class="custom-responsive-sm">{{ $property->mq }}</td>
-            <td class="custom-responsive-sm">@if($property->visible) <i class="fa-solid fa-check text-success"></i> @else <i class="fa-solid fa-xmark text-danger"></i> @endif</td>
-            <td class="custom-responsive-sm w-25">{{ $property->address }}</td>
-            <td>
-              <a href="{{ route("admin.properties.show", $property->slug) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
-              <a href="{{ route("admin.properties.edit", $property->slug) }}" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
-              <form class="d-inline delete-element" action="{{ route("admin.properties.destroy", $property->slug) }}" method="POST" data-element-name="{{ $property->title }}">
-                @csrf
-                @method("DELETE")
-                <button type="submit" class="btn btn-danger" value="delete"><i class="fa-solid fa-trash-can-arrow-up"></i><span class="d-none d-lg-inline">Delete</span></button>
-              </form>
-            </td>
+      @forelse ($properties as $property)
+        <tr class="{{ (!$property->visible) ? 'ms_not-visible' : ''}}">
+          <td>{{ $property->title }}</td>
+          <td class="custom-responsive-sm">{{ $property->night_price }}</td>
+          <td class="custom-responsive-sm">{{ $property->n_beds }}</td>
+          <td class="custom-responsive-sm">{{ $property->n_rooms }}</td>
+          <td class="custom-responsive-sm">{{ $property->mq }}</td>
+          <td class="custom-responsive-sm">@if($property->visible) <i class="fa-solid fa-check text-success"></i> @else <i class="fa-solid fa-xmark text-danger"></i> @endif</td>
+          <td class="custom-responsive-sm w-25">{{ $property->address }}</td>
+          <td>
+            <a href="{{ route("admin.properties.show", $property->slug) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
+            <a href="{{ route("admin.properties.edit", $property->slug) }}" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
+            <form class="d-inline delete-element" action="{{ route("admin.properties.destroy", $property->slug) }}" method="POST" data-element-name="{{ $property->title }}">
+              @csrf
+              @method("DELETE")
+              <button type="submit" class="btn btn-danger" value="delete"><i class="fa-solid fa-trash-can-arrow-up"></i><span class="d-none d-lg-inline">Delete</span></button>
+            </form>
+          </td>
         </tr>
-      @endforeach
+      @empty
+        <p class="m-3 fs-4">Il tuo profilo non ha proprietà vai sulla home per aggiungerne una</p>
+      @endforelse 
     </tbody>
   </table>
 </div>
