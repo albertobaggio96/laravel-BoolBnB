e8177dde2507a4e587730fc7213ab89907f090e0
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-30 22:24:48 +0200

    add back

--- resources/views/admin/properties/messages.blade.php
+++ resources/views/admin/properties/messages.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="return">
+   <a href="{{ route('admin.properties.index') }}" class="btn btn-primary m-2"><i class="fa-solid fa-arrow-left-long"></i></a>
+ </div>
 
 <section class="container-fluid show-container">
    <div class="row justify-content-center">
