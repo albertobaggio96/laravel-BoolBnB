e795d78562840f1d4288e5c5267b676292974677
cerry24 <samuelecerretti@gmail.com>
2023-03-31 12:30:05 +0200

    add layout for single message view

--- resources/views/admin/properties/message.blade.php
+++ resources/views/admin/properties/message.blade.php
@@ -2,13 +2,31 @@
 
 @section('content')
     <section>
-      <div class="return">
-        <a href="{{ route($redirect === 'index' ? 'admin.messages.index' : 'admin.properties.messages', $propertySlug) }}" class="btn btn-primary m-2"><i class="fa-solid fa-arrow-left-long"></i></a>
+      
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-12">
+            <div class="return">
+              <a href="{{ route($redirect === 'index' ? 'admin.messages.index' : 'admin.properties.messages', $propertySlug) }}" class="btn btn-primary m-2"><i class="fa-solid fa-arrow-left-long"></i></a>
+            </div>
+          </div>
+          <div class="col-sm-12 col-lg-8">
+            <div class="card my-card my-2 m-md-4 p-2 m-lg-5">
+              <div class="card-header ms_bg-violet">
+                <p class="text-white fs-5">Messaggio da: {{ $message->name }}</p>
+              </div>
+              <div class="card-body">
+                <h3 class="mb-4">{{ $message->subject }}</h3>
+                <div class="card-text p-1">
+                  {{ $message->body_message }}
+                </div>
+              </div>
+              <div class="card-footer">
+                <p>L'ospite vuole essere ricontattato alla seguente e-mail {{ $message->mail_from }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1> Proprietà: {{ $message->property->title }}</h1>
-      <div> nome: {{ $message->name }}</div>
-      <div> email: {{ $message->mail_from }}</div>
-      <div> oggetto: {{ $message->subject }} </div>
-      <div> testo: {{ $message->body_message }}</div>
     </section>
 @endsection
