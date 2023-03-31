881b27e03cb6bc045905cd1661f4348c674a0007
cerry24 <samuelecerretti@gmail.com>
2023-03-31 11:25:23 +0200

    change style for read messages + add object to message list

--- resources/views/admin/properties/messages.blade.php
+++ resources/views/admin/properties/messages.blade.php
@@ -3,28 +3,32 @@
 @section('content')
 <div class="return">
    <a href="{{ route('admin.properties.index') }}" class="btn btn-primary m-2"><i class="fa-solid fa-arrow-left-long"></i></a>
- </div>
+</div>
 
 <section class="container-fluid show-container">
    <div class="row justify-content-center">
       <div class="col-sm-12 col-lg-8">
          <h1 class="m-2 text-center">
-            Ecco i messaggi della proprietà
+            Ecco i messaggi della proprietà 
          </h1>
          @foreach ($messages as $message)
-         <article class="card my-card my-2 m-md-4 p-2 m-lg-5 {{ $message->displayed ? 'bg-secondary' : '' }}">
+         <article class="card my-card my-2 m-md-4 p-2 m-lg-5 {{ $message->displayed ? 'ms_not-visible' : '' }}">
                      
             <a href="{{ route('admin.messages.displayed', [$message->id, 'messages']) }}" class="card-info text-decoration-none text-black">
                <div class="mb-3">
-                   <h2 class="fs-5 d-inline">Nome :</h2>
-                   <span>{{$message->name}}</span>   
+                  <h2 class="fs-5 d-inline">Nome :</h2>
+                  <span>{{$message->name}}</span>   
+               </div>
+               <div class="mb-3">
+                  <h3 class="fs-5 d-inline">Proprietà: </h3>
+                  <span>{{ $message->property->title }}</span>
                </div>
                <div>
-                   <h3 class="fs-5 d-inline">Proprietà: </h3>
-                   <span>{{ $message->property->title }}</span>
+                  <h4 class="fs-5 d-inline">Oggetto: </h3>
+                  <span>{{ $message->subject }}</span>
                </div>
             </a>
-    
+
             <form class="d-flex delete-element justify-content-end" action="{{ route('admin.messages.destroy', [$message['id'],'messages']) }}" method="POST" data-element-name="">
                @csrf
                @method("DELETE")
@@ -39,7 +43,7 @@
 @endsection
 
 @section("js")
-  {{-- popup --}}
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  @vite('resources/js/deleteConfirm.js')
+   {{-- popup --}}
+   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   @vite('resources/js/deleteConfirm.js')
 @endsection
